f9ed11df49483f9d283d11362be549f511aefec0
Umi <Umi@umis-mbp.home>
2019-09-10 20:45:13 +0100

    added facebook auth model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20190418182804) do
 
   create_table "photos", force: :cascade do |t|
@@ -44,6 +45,9 @@ ActiveRecord::Schema.define(version: 20190418182804) do
     t.datetime "updated_at",   null: false
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
+=======
+ActiveRecord::Schema.define(version: 20190414181140) do
+>>>>>>> 654337b... added facebook auth model
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -60,8 +64,11 @@ ActiveRecord::Schema.define(version: 20190418182804) do
     t.string   "provider"
     t.string   "uid"
     t.string   "image"
+<<<<<<< HEAD
     t.string   "phone_number"
     t.text     "description"
+=======
+>>>>>>> 654337b... added facebook auth model
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
